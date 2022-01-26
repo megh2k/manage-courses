135f20051bff7a1c8307a9e57bad75239e6800c6
Megh Patel <86826735+megh2k@users.noreply.github.com>
2022-01-26 09:23:04 -0500

    link changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light">
       <Link to="/manage-courses" className="navbar-brand brand1">
-        <h1>Change</h1>
+        <h1>Changesss</h1>
       </Link>
       <Link to="/manage-courses/all" className="navbar-brand brand2">
         All Courses
