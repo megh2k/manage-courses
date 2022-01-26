b29d317a831108d3adb4f576716839fdafb5c48b
Megh Patel <86826735+megh2k@users.noreply.github.com>
2022-01-25 23:59:02 -0500

    Links updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light">
-      <Link to="/" className="navbar-brand brand1">
+      <Link to="/manage-courses" className="navbar-brand brand1">
         <h1>Courses</h1>
       </Link>
-      <Link to="/all" className="navbar-brand brand2">
+      <Link to="/manage-courses/all" className="navbar-brand brand2">
         All Courses
       </Link>
-      <Link to="/add" className="navbar-brand brand3">
+      <Link to="/manage-courses/add" className="navbar-brand brand3">
         Add Course
       </Link>
     </nav>
