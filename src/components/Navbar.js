021e4c694d37e29a8ac4877c0327fd35083b8854
Megh Patel <86826735+megh2k@users.noreply.github.com>
2022-01-25 23:55:23 -0500

    Links added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  return(
+  return (
     <nav className="navbar navbar-expand-lg navbar-light">
-        <a className="navbar-brand brand1" href="/"><h1>Courses</h1></a> 
-        <a className="navbar-brand brand2" href="/all">All Courses</a> 
-        <a className="navbar-brand brand3" href="/add">Add Course</a> 
-
-      
-  </nav>);
+      <Link to="/" className="navbar-brand brand1">
+        <h1>Courses</h1>
+      </Link>
+      <Link to="/all" className="navbar-brand brand2">
+        All Courses
+      </Link>
+      <Link to="/add" className="navbar-brand brand3">
+        Add Course
+      </Link>
+    </nav>
+  );
 }
