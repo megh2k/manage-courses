cf35c4e461d1037fc290fe20736d8512f99a7c11
Megh Patel <86826735+megh2k@users.noreply.github.com>
2022-01-25 20:57:33 -0500

    x

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Navbar() {
   return(
     <nav className="navbar navbar-expand-lg navbar-light">
-        <a className="navbar-brand brand1" href="manage-courses/"><h1>Courses</h1></a> 
-        <a className="navbar-brand brand2" href="manage-courses/all">All Courses</a> 
-        <a className="navbar-brand brand3" href="manage-courses/add">Add Course</a> 
+        <a className="navbar-brand brand1" href="/"><h1>Courses</h1></a> 
+        <a className="navbar-brand brand2" href="/all">All Courses</a> 
+        <a className="navbar-brand brand3" href="/add">Add Course</a> 
 
       
   </nav>);
